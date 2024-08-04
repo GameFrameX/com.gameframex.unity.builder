be2864980e2c3c2498713d8f4ffa9d50b2760205
Blank <wangfj11@foxmail.com>
2024-08-04 22:33:16 +0800

    [修改]1. 修改默认值的设置方式

--- Editor/BuilderOptions.cs
+++ Editor/BuilderOptions.cs
@@ -37,22 +37,22 @@ namespace GameFrameX.Builder.Editor
         /// <summary>
         /// 是否上传日志文件
         /// </summary>
-        public bool IsUploadLogFile { get; set; }
+        public bool IsUploadLogFile { get; set; } = false;
 
         /// <summary>
         /// 资源包名称。只有调用BuildAsset 的时候有效
         /// </summary>
-        public string PackageName { get; set; }
+        public string PackageName { get; set; } = "DefaultPackage";
 
         /// <summary>
         /// 是否使用增量构建的方式构建。只有调用BuildAsset 的时候有效
         /// </summary>
-        public bool IsIncrementalBuildPackage { get; set; }
+        public bool IsIncrementalBuildPackage { get; set; } = false;
 
         /// <summary>
         /// 是否上传资源
         /// </summary>
-        public bool IsUploadAsset { get; set; }
+        public bool IsUploadAsset { get; set; } = false;
 
         /// <summary>
         /// 上传资源存储路径
@@ -62,17 +62,7 @@ namespace GameFrameX.Builder.Editor
         /// <summary>
         /// 是否上传APK,只有调用BuildApk 的时候有效
         /// </summary>
-        public bool IsUploadApk { get; set; }
-
-        public BuilderOptions()
-        {
-            PackageName = "DefaultPackage";
-            IsUploadLogFile = false;
-            IsIncrementalBuildPackage = false;
-            IsUploadAsset = false;
-            IsUploadApk = false;
-            UploadAssetSavePath = string.Empty;
-        }
+        public bool IsUploadApk { get; set; } = false;
 
         public override string ToString()
         {
